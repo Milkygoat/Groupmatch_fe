b1dcde0d4a88aef42475f4143e2ab55f01157084
Mr.Harry <harryprasad@student.telkomuniversity.ac.id>
2026-05-31 20:51:22 +0700

    fix: Always include Railway domains in CORS allowed_origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -43,11 +43,9 @@ app = FastAPI(debug=True)
 allowed_origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://groupmatchfe-production.up.railway.app",
+    "https://groupmatchfe.up.railway.app"
 ]
-
-if ENV == "production":
-    allowed_origins.append("https://groupmatchfe-production.up.railway.app")
-    allowed_origins.append("https://groupmatchfe.up.railway.app")
 
 app.add_middleware(
     CORSMiddleware,
